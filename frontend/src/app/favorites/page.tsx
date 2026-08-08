d551fb3de79e97fe65d5251c416bd9792169e741
hika357 <jpip010688@gmail.com>
2026-08-08 16:55:43 +0900

    CSSつけた

--- frontend/src/app/favorites/page.tsx
+++ frontend/src/app/favorites/page.tsx
@@ -1,17 +1,23 @@
 "use client";
 import axios from 'axios';
 import { useEffect } from "react";
-import FavoriteCard from '../../components/Favoritecard';
+import  FavoriteCard from '../../components/FavoriteCard';
 export default function FavoritesPage() {
   const getFavoritesList = () => {useEffect(() => {async () => { await axios.get("/api/favorites/list").then(response => {return response.data})};}, [])};
 
   return (
     <div className="px-5 pt-6">
-      <FavoriteCard data={getFavoritesList} />
-      <h1 className="text-lg font-bold mb-4">お気に入り</h1>
-      <p className="text-sm" style={{ color: "var(--color-text-muted)" }}>
+       {/* タイトル */}
+      <h1 className="text-3xl font-bold text-center mb-8">♡お気に入りレシピ♡</h1>
+      <div className="gap-4 flex justify-center"><FavoriteCard data={getFavoritesList} /><FavoriteCard data={getFavoritesList} /><FavoriteCard data={getFavoritesList} /><FavoriteCard data={getFavoritesList} /><FavoriteCard data={getFavoritesList} /><FavoriteCard data={getFavoritesList} /><FavoriteCard data={getFavoritesList} /><FavoriteCard data={getFavoritesList} /><FavoriteCard data={getFavoritesList} /><FavoriteCard data={getFavoritesList} /></div>
+     
+      {/* レシピ一覧 */}
+      <div className="grid grid-cols-2 lg:grid-cols-3 gap-6"></div>
+
+           <p className="text-sm" style={{ color: "var(--color-text-muted)" }}>
         お気に入りに保存したレシピがここに表示されます。
       </p>
+     
     </div>
   );
 }
--- frontend/src/components/Favoritecard.tsx
+++ frontend/src/components/Favoritecard.tsx
@@ -4,6 +4,41 @@ import Link from "next/link";
 
 export default function FavoriteCard({ data }: { data: any }) {
   return (
-    <><h1>test</h1></>
-  );
+  <div className="gap-4 p-1">    
+
+        {/* レシピカード */}
+        <div className="bg-white rounded-3xl w-70 shadow-md overflow-hidden hover:shadow-xl transition duration-300">
+          {/* 画像 */}
+          <img src="img/" alt="カルボナーラ" className="w-70 h-52 object-cover"/>
+
+          {/* 内容 */}
+          <div className="p-5">
+            {/* タイトル+ハート */}
+            <div className="flex justify-between items-center">
+              <h2 className="text-xl font-bold">カルボナーラ</h2>
+
+              <button>
+                <p className="text-red-500 fill-red-500 size={28}"></p>
+              </button>
+           </div>
+            
+            {/* 食材 */}
+            <p className="text-gray-500 mt-3">
+              ベーコン・卵・牛乳
+            </p>
+
+            {/* 調理時間 */}
+            <div className="mt-4">
+              <span className="bg-orange-100 text-orange-600 px-3 py-1 rounded-full text-sm">
+                15分
+              </span>
+              <button className="heart">♡Favorites</button>
+            </div>
+          </div>
+        </div>      
+      </div>   
+
+ 
+    );
+
 }
